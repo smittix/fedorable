7da749dfb5d53750f3840836af8727bae5cdadca
Osiris <jamessmith1@outlook.com>
2021-10-29 07:44:13 +0100

    Add files via upload

---
+++ fedora-setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+HEIGHT=15
+WIDTH=90
+CHOICE_HEIGHT=4
+BACKTITLE="Fedora Setup Util - By Osiris - https://stealingthe.network"
+TITLE="Make a selection"
+MENU="Please Choose one of the following options:"
+
+
+
+
+OPTIONS=(1 "Enable RPM Fusion - Enables the RPM Fusion Repos"
+         2 "Enable Better Fonts - Better font rendering by Dawid"
+         3 "Speed up DNF - This enables fastestmirror, max downloads and deltarpms"
+         4 "Enable Flatpak - Flatpak is installed by default but not enabled"
+         5 "Install Software - Installs a bunch of my most used software"
+         6 "Setup Flat Look - Installs and Enables the Flat GTK and Icon themes"
+         7 "Quit")
+
+while [ "$CHOICE -ne 4" ]; do
+    CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --nocancel \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+    clear
+    case $CHOICE in
+        1)  echo "Enabling RPM Fusion"
+            sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            notify-send "RPM Fusion Enabled" --expire-time=10
+           ;;
+        2)  echo "Enabling Better Fonts by Dawid"
+            sudo -s dnf copr enable dawid/better_fonts
+            sudo -s dnf install fontconfig-font-replacements
+            sudo -s dnf install fontconfig-enhanced-defaults
+            notify-send "Fonts prettified - enjoy!" --expire-time=10
+           ;;
+        3)  echo "Speeding Up DNF"
+            echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+            echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+            echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+            notify-send "Your DNF config has now been amended" --expire-time=10
+           ;;
+        4)  echo "Enabling Flatpak"
+            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            flatpak update
+            notify-send "Flatpak has now been enabled" --expire-time=10
+           ;;
+        5)  echo "Installing Software"
+            sudo dnf install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel install lame\* --exclude=lame-devel elementary-icon-theme deepin-icon-theme yaru-theme numix-gtk-theme moka-icon-theme greybird-dark-theme arc-theme tlp tlp-rdw vlc dropbox nautilus-dropbox dnfdragora paper-icon-theme flat-remix-icon-theme flat-remix-theme
+            notify-send "Software has been installed" --expire-time=10
+           ;;
+        6)  echo "Enabling Flat GTK and Icon Theme"
+            gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue"
+            gsettings set org.gnome.desktop.wm.preferences theme "Flat-Remix-Blue"
+            gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue'
+            notify-send "There you go, that's better" --expire-time=10
+           ;;
+        7)
+          exit 0
+          ;;
+    esac
+done
