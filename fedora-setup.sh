3f8f5eee8cb15c96e2cf0fac55c8c132c8d1b6b1
Osiris <jamessmith1@outlook.com>
2022-06-21 13:05:06 +0100

    update fedora-setup.sh
    Cleaned script to remove some old no longer needed apps

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -3,8 +3,8 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 HEIGHT=20
 WIDTH=90
 CHOICE_HEIGHT=4
-BACKTITLE="Fedora Setup Util - By Osiris - https://stealingthe.network"
-TITLE="Make a selection"
+BACKTITLE="Fedora Setup Util - By Osiris - https://lsass.co.uk"
+TITLE="Please Make a selection"
 MENU="Please Choose one of the following options:"
 
 #Other variables
@@ -15,16 +15,14 @@ if [ $(rpm -q dialog 2>/dev/null | grep -c "is not installed") -eq 1 ]; then
 sudo dnf install -y dialog
 fi
 
-OPTIONS=(1 "Enable RPM Fusion - Enables the RPM Fusion Repos"
+OPTIONS=(1 "Enable RPM Fusion - Enables the RPM Fusion repos for your specific version"
          2 "Enable Better Fonts - Better font rendering by Dawid"
          3 "Speed up DNF - This enables fastestmirror, max downloads and deltarpms"
          4 "Enable Flatpak - Enables the Flatpak repository"
-         5 "Install Common Software - Installs a bunch of my most used software"
-         6 "Enable Flat Theme - Installs and Enables the Flat GTK and Icon themes"
-         7 "Install Oh-My-ZSH"
-         8 "Enable Tweaks, Extensions & Plugins"
-	 9 "Install Microsoft Edge"
-         10 "Quit")
+         5 "Install Common Software - Installs a bunch of my most used software (see git)"
+         6 "Install Oh-My-ZSH"
+         7 "Enable Tweaks, Extensions & Plugins"
+	 8 "Quit")
 
 while [ "$CHOICE -ne 4" ]; do
     CHOICE=$(dialog --clear \
@@ -64,40 +62,29 @@ while [ "$CHOICE -ne 4" ]; do
             notify-send "Flatpak has now been enabled" --expire-time=10
            ;;
         5)  echo "Installing Software"
-            sudo dnf install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator vlc dropbox nautilus-dropbox dnfdragora audacious mscore-fonts-all neofetch cmatrix p7zip unzip gparted
+            sudo dnf install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator vlc dropbox nautilus-dropbox dnfdragora audacious mscore-fonts-all neofetch cmatrix p7zip unzip gparted alacritty nikto nmap fira-code-fonts
             notify-send "Software has been installed" --expire-time=10
            ;;
-        6)  echo "Installing Appearance Tweaks - Flat GTK and Icon Theme"
-            sudo dnf install -y gnome-shell-extension-user-theme paper-icon-theme flat-remix-icon-theme flat-remix-theme
-            gnome-extensions install user-theme@gnome-shell-extensions.gcampax.github.com
-            gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-            gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue"
-            gsettings set org.gnome.desktop.wm.preferences theme "Flat-Remix-Blue"
-            gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue'
-            notify-send "There you go, that's better" --expire-time=10
-           ;;
-        7)  echo "Installing Oh-My-Zsh"
+        6)  echo "Installing Oh-My-Zsh"
             sudo dnf -y install zsh util-linux-user
             sh -c "$(curl -fsSL $OH_MY_ZSH_URL)"
             echo "change shell to ZSH"
             chsh -s "$(which zsh)"
             notify-send "Oh-My-Zsh is ready to rock n roll" --expire-time=10
            ;;
-        8)  echo "Installing Tweaks, extensions & plugins"
+        7)  echo "Installing Tweaks, extensions & plugins"
             sudo dnf groupupdate -y sound-and-video
             sudo dnf install -y libdvdcss
             sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
             sudo dnf install -y lame\* --exclude=lame-devel
             sudo dnf group upgrade -y --with-optional Multimedia
+	    sudo dnf install -y jetbrains-mono-fonts-all
+	    sudo dnf copr enable peterwu/iosekva -y
+	    sudo dnf search iosevka
+	    sudo dnf install iosevka-term-fonts
             notify-send "All done" --expire-time=10
-           ;;
-	 9) echo "Installing microsoft edge"
-            sudo rpm -v --import https://packages.microsoft.com/keys/microsoft.asc
-            sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-            sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge.repo
-            sudo dnf install -y microsoft-edge-stable
 	   ;;
-        10)
+        8)
           exit 0
           ;;
     esac
