1160b3ec3596743aab46e5387c69a959850fbd4a
Smittix <jamessmith1@outlook.com>
2023-12-07 23:18:06 +0000

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -11,6 +11,9 @@ gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds false
 gsettings set org.gnome.desktop.interface clock-show-weekday false
 
+# Enables battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
 # Enable window buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
