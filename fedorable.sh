21a2daba247bbfda78fdaf369a0edf673848e5eb
Smittix <jamessmith1@outlook.com>
2024-05-23 11:01:27 +0100

    Update fedorable.sh
    Clean up of code, logging added.

--- fedorable.sh
+++ fedorable.sh
@@ -1,31 +1,148 @@
 #!/bin/bash
+
+# Set PATH
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+
+# Dialog dimensions
 HEIGHT=20
 WIDTH=90
-CHOICE_HEIGHT=4
-BACKTITLE="Fedorable a Fedora Post Install Setup Util for GNOME - By Smittix - https://lsass.co.uk"
-TITLE="Please Make a selection"
+CHOICE_HEIGHT=10
+
+# Titles and messages
+BACKTITLE="Fedorable - A Fedora Post Install Setup Util for GNOME - By Smittix - https://lsass.co.uk"
+TITLE="Please Make a Selection"
 MENU="Please Choose one of the following options:"
 
-#Other variables
+# Other variables
 OH_MY_ZSH_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+LOG_FILE="setup_log.txt"
 
-#Check to see if Dialog is installed, if not install it - Thanks Kinkz_nl
-if [ $(rpm -q dialog 2>/dev/null | grep -c "is not installed") -eq 1 ]; then
-sudo dnf install -y dialog
+# Log function
+log_action() {
+    local message=$1
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" | tee -a $LOG_FILE
+}
+
+# Check for dialog installation
+if ! rpm -q dialog &>/dev/null; then
+    sudo dnf install -y dialog || { log_action "Failed to install dialog. Exiting."; exit 1; }
+    log_action "Installed dialog."
 fi
 
-OPTIONS=(1 "Enable RPM Fusion - Enables the RPM Fusion repos for your specific version"
-         2 "Update Firmware - If your system supports FW update delivery"
-         3 "Speed up DNF - Sets max parallel downloads to 10"
-         4 "Enable Flatpak - Enables the Flatpak repo and installs packages located in flatpak-packages.txt"
-         5 "Install Software - Installs software located in dnf-packages.txt"
-         6 "Install Oh-My-ZSH - Installs Oh-My-ZSH & Starship Prompt"
-         7 "Install Extras - Themes Fonts and Codecs"
-         8 "Install Nvidia - Install akmod Nvidia drivers"
-	     9 "Quit")
+# Options for the menu
+OPTIONS=(
+    1 "Enable RPM Fusion - Enables the RPM Fusion repos for your specific version"
+    2 "Update Firmware - If your system supports FW update delivery"
+    3 "Speed up DNF - Sets max parallel downloads to 10"
+    4 "Enable Flatpak - Enables the Flatpak repo and installs packages located in flatpak-packages.txt"
+    5 "Install Software - Installs software located in dnf-packages.txt"
+    6 "Install Oh-My-ZSH - Installs Oh-My-ZSH & Starship Prompt"
+    7 "Install Extras - Themes, Fonts, and Codecs"
+    8 "Install Nvidia - Install akmod Nvidia drivers"
+    9 "Quit"
+)
 
-while [ "$CHOICE -ne 4" ]; do
+# Function to display notifications
+notify() {
+    local message=$1
+    local expire_time=${2:-10}
+    if command -v notify-send &>/dev/null; then
+        notify-send "$message" --expire-time="$expire_time"
+    fi
+    log_action "$message"
+}
+
+# Function to handle RPM Fusion setup
+enable_rpm_fusion() {
+    echo "Enabling RPM Fusion"
+    sudo dnf install -y \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf upgrade --refresh -y
+    sudo dnf groupupdate -y core
+    sudo dnf install -y rpmfusion-free-release-tainted dnf-plugins-core
+    notify "RPM Fusion Enabled"
+}
+
+# Function to update firmware
+update_firmware() {
+    echo "Updating System Firmware"
+    sudo fwupdmgr get-devices
+    sudo fwupdmgr refresh --force
+    sudo fwupdmgr get-updates
+    sudo fwupdmgr update
+    notify "System Firmware Updated"
+}
+
+# Function to speed up DNF
+speed_up_dnf() {
+    echo "Speeding Up DNF"
+    echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+    notify "Your DNF config has now been amended"
+}
+
+# Function to enable Flatpak
+enable_flatpak() {
+    echo "Enabling Flatpak"
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak update -y
+    if [ -f flatpak-install.sh ]; then
+        source flatpak-install.sh
+    else
+        log_action "flatpak-install.sh not found"
+    fi
+    notify "Flatpak has now been enabled"
+}
+
+# Function to install software
+install_software() {
+    echo "Installing Software"
+    if [ -f dnf-packages.txt ]; then
+        sudo dnf install -y $(cat dnf-packages.txt)
+        notify "Software has been installed"
+    else
+        log_action "dnf-packages.txt not found"
+    fi
+}
+
+# Function to install Oh-My-Zsh and Starship
+install_oh_my_zsh() {
+    echo "Installing Oh-My-Zsh with Starship"
+    sudo dnf install -y zsh curl util-linux-user
+    sh -c "$(curl -fsSL $OH_MY_ZSH_URL)" "" --unattended
+    chsh -s "$(which zsh)"
+    curl -sS https://starship.rs/install.sh | sh
+    echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+    notify "Oh-My-Zsh is ready to rock n roll"
+}
+
+# Function to install extras
+install_extras() {
+    echo "Installing Extras"
+    sudo dnf groupupdate -y sound-and-video
+    sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+    sudo dnf install -y libdvdcss
+    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+    sudo dnf install -y lame\* --exclude=lame-devel
+    sudo dnf group upgrade -y --with-optional Multimedia
+    sudo dnf config-manager --set-enabled fedora-cisco-openh264
+    sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
+    sudo dnf copr enable peterwu/iosevka -y
+    sudo dnf update -y
+    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-noto-fonts-common fira-code-fonts cabextract xorg-x11-font-utils fontconfig
+    sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+    notify "All done"
+}
+
+# Function to install Nvidia drivers
+install_nvidia() {
+    echo "Installing Nvidia Driver Akmod-Nvidia"
+    sudo dnf install -y akmod-nvidia
+    notify "Please wait 5 minutes until rebooting"
+}
+
+# Main loop
+while true; do
     CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
@@ -37,76 +154,15 @@ while [ "$CHOICE -ne 4" ]; do
 
     clear
     case $CHOICE in
-        1)  echo "Enabling RPM Fusion"
-            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-	    sudo dnf upgrade --refresh
-            sudo dnf groupupdate -y core
-            sudo dnf install -y rpmfusion-free-release-tainted
-            sudo dnf install -y dnf-plugins-core
-            notify-send "RPM Fusion Enabled" --expire-time=10
-           ;;
-        2)  echo "Updating System Firmware"
-            #Updates firmware where available
-            sudo fwupdmgr get-devices 
-            sudo fwupdmgr refresh --force 
-            sudo fwupdmgr get-updates 
-            sudo fwupdmgr update
-           ;;
-        3)  echo "Speeding Up DNF"
-            #Sets max parallel downloads to 10
-            echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-            notify-send "Your DNF config has now been amended" --expire-time=10
-           ;;
-        4)  echo "Enabling Flatpak"
-            #Enables the flatpak repository and installs packages located within flatpak-packages.txt
-            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-            flatpak update
-            source flatpak-install.sh
-            notify-send "Flatpak has now been enabled" --expire-time=10
-           ;;
-        5)  echo "Installing Software"
-            #Installs packages that are listed in the dnf-packages.txt file
-            sudo dnf install -y $(cat dnf-packages.txt)
-            notify-send "Software has been installed" --expire-time=10
-           ;;
-        6)  echo "Installing Oh-My-Zsh with Starship"
-            #Installs Oh-My-ZSH along with the Starship prompt
-            sudo dnf -y install zsh curl util-linux-user
-            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-            echo "change shell to ZSH"
-            chsh -s "$(which zsh)"
-            curl -sS https://starship.rs/install.sh | sh
-            notify-send "Oh-My-Zsh is ready to rock n roll" --expire-time=10
-            echo 'eval "$(starship init zsh)"' >> ~/.zshrc
-            notify-send "Oh-My-Zsh is ready to rock n roll" --expire-time=10
-           ;;
-        7)  echo "Installing Extras"
-            #Media Codecs
-            sudo dnf groupupdate -y sound-and-video
-            sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-            sudo dnf install -y libdvdcss
-            sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-            sudo dnf install -y lame\* --exclude=lame-devel
-            sudo dnf group upgrade -y --with-optional Multimedia
-	    sudo dnf config-manager --set-enabled fedora-cisco-openh264
-            sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
-	    #Iosevka Font Copr
-            sudo dnf copr enable peterwu/iosevka -y
-            sudo dnf update -y
-	    #Themes and Icons
-            sudo dnf install -y papirus-icon-theme gnome-shell-theme-flat-remix gnome-shell-theme-yaru 
-            #Fonts
-     	    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-noto-fonts-common fira-code-fonts cabextract xorg-x11-font-utils fontconfig
-            #Microsoft Core Fonts
-            sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-            notify-send "All done" --expire-time=10
-           ;;
-        8)  echo "Installing Nvidia Driver Akmod-Nvidia"
-            sudo dnf install -y akmod-nvidia
-            notify-send "Please wait 5 minutes until rebooting" --expire-time=10
-	       ;;
-        9)
-          exit 0
-          ;;
+        1) enable_rpm_fusion ;;
+        2) update_firmware ;;
+        3) speed_up_dnf ;;
+        4) enable_flatpak ;;
+        5) install_software ;;
+        6) install_oh_my_zsh ;;
+        7) install_extras ;;
+        8) install_nvidia ;;
+        9) log_action "User chose to quit the script."; exit 0 ;;
+        *) log_action "Invalid option selected: $CHOICE";;
     esac
 done
